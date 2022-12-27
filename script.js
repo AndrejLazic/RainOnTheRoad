3a95712bb321457477d0dd31aa3ee133f0d3804e
Andrej Lazic <ane.lazic@gmail.com>
2022-12-27 18:15:13 +0100

    radi bojanje kvadratica (prosledjivao string umesto int)

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
+counting_white()
+
 function coloring_black(cell_id){
     const gamecells = document.querySelectorAll(".gamecell");
-        for(let i = cell_id; i<gamecells.length; i+=8){
+        for(let i = cell_id; i < gamecells.length; i = i + 8){
             gamecells[i].style.backgroundColor = "black";
         }
+        counting_white()
 }
-coloring_black(1);
 
 function coloring_white(cell_id){
     const gamecells = document.querySelectorAll(".gamecell");
-    //console.log(gamecells.length);
-            for(let i = cell_id; i<40; i=i+8){
-                gamecells[i].style.backgroundColor = "whitesmoke";
-            }
-            counting_white()
+        for(let i = cell_id; i >= 0; i = i - 8){
+            gamecells[i].style.backgroundColor = "whitesmoke";
+        }
+        counting_white()
 }
 
 document.querySelectorAll(".gamecell").forEach(item => {
     item.addEventListener('click', function () {
-        console.log(item);
-        i = item.id;
+        i = parseInt(item.id);
         if(item.style.backgroundColor == "black"){
         coloring_white(i);
         }else{
@@ -30,26 +30,10 @@ document.querySelectorAll(".gamecell").forEach(item => {
 function counting_white(){
     const gamecells = document.querySelectorAll(".gamecell");
     var white = 0;
-    //console.log(gamecells[1]);
-        for(let i = 0; i<gamecells.length; i++){
+        for(let i = 0; i < gamecells.length; i++){
             if(gamecells[i].style.backgroundColor == "whitesmoke"){
                 white++;
             }
         }
         console.log("belih", white);
 }
-/*
-document.querySelectorAll(".gamecell").forEach(item => {
-    item.addEventListener('mouseover', function () {
-        console.log(item.id);
-        coloring_black(item.id);
-    })
-})
-
-document.querySelectorAll(".gamecell").forEach(item => {
-    item.addEventListener('mouseout', function () {
-        console.log(item.id);
-        coloring_white(item.id);
-    })
-})
-*/
